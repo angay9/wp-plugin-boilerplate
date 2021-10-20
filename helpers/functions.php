c4b4a7b3735e52a919299e9cdd2058a7c3f28f05
Andriy Haydash <angay9@gmail.com>
2021-10-20 10:10:53 +0200

    ADD get user ip function

--- helpers/functions.php
+++ helpers/functions.php
@@ -131,3 +131,25 @@ if (! function_exists('pb_user_has_role')) {
         return $has_role;
     }
 }
+
+if (! function_exists('pb_get_user_ip')) {
+    function pb_get_user_ip() {
+        $ip_addr = null;
+
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key)
+        {
+            if (array_key_exists($key, $_SERVER) === true)
+            {
+                foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip)
+                {
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false)
+                    {
+                        return $ip;
+                    }
+                }
+            }
+        }
+
+        return $ip_addr;
+    }
+}
